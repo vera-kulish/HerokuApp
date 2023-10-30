ee197d62aa55646160d57195c831da22f9b85809
vera-kulish <verakoulich@gmail.com>
2023-10-30 19:18:51 +0100

    TMS-1 add/remove test

---
+++ src/test/java/AddRemoveElementsTest.java
@@ -0,0 +1,42 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class AddRemoveElementsTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void addRemoveElements() {
+        driver.get("https://the-internet.herokuapp.com/add_remove_elements/");
+        driver.findElement(By.xpath("//button[text()='Add Element']")).click();
+        driver.findElement(By.xpath("//button[text()='Add Element']")).click();
+        driver.findElements(By.xpath("//button[text()='Delete']")).get(1).click();
+
+        Assert.assertEquals(driver.findElements(By.xpath("//button[text()='Delete']")).size(), 1);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void close() {
+        driver.quit();
+    }
+}
